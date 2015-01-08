1b341bdb40b9b8a2616030b155e3e4b7dcf78726
Sean Hayes <sean@seanhayes.name>
2015-01-08 16:56:41 -0500

    Changes to get setup.py working.

--- django_testmigrate/management/commands/testmigrate.py
+++ django_testmigrate/management/commands/testmigrate.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.core.management.base import NoArgsCommand, CommandError
 
-from ... import TestMigrationRunner
+from ...base import TestMigrationRunner
 
 
 class Command(NoArgsCommand):
